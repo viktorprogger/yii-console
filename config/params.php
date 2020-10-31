d4c3fa3a94cdac744cf787dbec58165b394f69a2
viktorprogger <viktorprogger@gmail.com>
2020-11-01 00:37:44 +0300

    Initial commit

--- config/params.php
+++ config/params.php
@@ -2,16 +2,4 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Yii\Console\Command\Serve;
-
-return [
-    'yiisoft/yii-console' => [
-        'id' => 'yii-console',
-        'name' => 'Yii Console',
-        'commands' => [
-            'serve' => Serve::class,
-        ],
-        'version' => '3.0',
-        'rebuildConfig' => static fn () => getenv('APP_ENV') === 'dev',
-    ],
-];
+return [];
